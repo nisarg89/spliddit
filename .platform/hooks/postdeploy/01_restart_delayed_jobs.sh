b4ee8c314541d5de2f1ff08139c6f6adcd3e393b
Soroush Ebadian <soroushebadian@gmail.com>
2024-02-29 12:37:11 -0500

    permissions for pid and workers, resourse_id allow nil

--- .platform/hooks/postdeploy/01_restart_delayed_jobs.sh
+++ .platform/hooks/postdeploy/01_restart_delayed_jobs.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-#. /opt/elasticbeanstalk/support/envvars
-#cd $EB_CONFIG_APP_CURRENT
-#su -c "chmod +x script/delayed_job"
-#su -c "bundle exec rake db:init_data" $EB_CONFIG_APP_USER
-#su -c "bundle exec rake db:migrate" $EB_CONFIG_APP_USER
-#su -c "RAILS_ENV=production script/delayed_job --pid-dir=$EB_CONFIG_APP_SUPPORT/pids -n 2 restart" $EB_CONFIG_APP_USER
+cd /var/app/current/ || exit
+su -c "chmod +x script/delayed_job"
+su webapp -c "RAILS_ENV=production bundle exec rake db:migrate"
+su webapp -c "RAILS_ENV=production script/delayed_job --pid-dir=/var/app/containerfiles/pids -n 2 restart"
+su webapp -c "RAILS_ENV=production bundle exec rake jobs:work"
---
+++ .platform/hooks/predeploy/03_pid_directory.sh
@@ -0,0 +1,3 @@
+sudo mkdir -p /var/app/containerfiles/pids
+sudo chown -R webapp:webapp /var/app/containerfiles/pids
+sudo chmod -R 755 /var/app/containerfiles/pids
