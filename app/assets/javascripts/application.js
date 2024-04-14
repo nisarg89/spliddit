a0074d8b5e812ca2436bc89e44b2d835ad33edaf
Nisarg Shah <nisarg89@gmail.com>
2024-04-14 00:32:14 -0400

    Reverting to SES signature v4 fix and upgrading to GA4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,12 +35,3 @@ function addToMailingList() {
   }
   return false;
 }
-
-// Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-55662510-1', 'auto');
-ga('send', 'pageview');
